99ac7414843175dc92e056ae89a879c9e7f7543b
Christine Humphries <cmh@chrishumphries.net>
2018-05-22 17:43:36 -0400

    finished project

---
+++ src/main/java/virtualPet/VirtualPet.java
@@ -0,0 +1,115 @@
+package virtualPet;
+
+public class VirtualPet {
+
+//	VirtualPet virtualPet = new VirtualPet("Grendel the Troll");
+
+	public String petName = "Grendel The Troll";
+	
+	public void displayStats() {
+		System.out.println("Grendel the Troll");
+		System.out.println("Bloodlust : " + bloodLust);
+		System.out.println("Health : " + health);
+		System.out.println("Bridge Tolls Collected: " + trollToll);
+		System.out.println("Wit: " + wit);
+	}
+
+	public int bloodLust = 0;
+	public int trollToll = 0;
+	// use currency for toll
+	public int wit = 50;
+	public int health = 100;
+
+	public void tick() {
+		bloodLust += 10;
+		health -= 5;
+		trollToll += 5;
+		
+		if (health < 0) {
+			System.out.println("You crawl under your bridge and die!");
+			System.exit(0);
+		}
+		if (bloodLust > 50) {
+			System.out.println("You're a monster! You strenghth begins to fade.");
+			health -= 10;
+		}
+		if(wit > 75) {
+			System.out.println("You use your cunning to collect more tolls.");
+			trollToll += 15; 
+		}
+		if(trollToll > 200) {
+			System.out.println("You collected enough tolls to retire!");
+			System.exit(0);
+		}
+	}
+
+	public VirtualPet() {
+
+	}
+
+	public VirtualPet(String string) {
+	}
+
+	public int getBloodLust() {
+		return bloodLust;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	private int getbloodLust() {
+		return bloodLust;
+	}
+
+	public int getTrollToll() {
+		return trollToll;
+	}
+
+	public int getincreaseBloodLust() {
+		bloodLust += 10;
+		return bloodLust;
+	}
+
+	public int getincreaseTrollToll() {
+		trollToll += 50;
+		return trollToll;
+	}
+
+	public int getdecreaseTrollToll() {
+		trollToll -= 25;
+		return trollToll;
+	}
+
+	public int getdecreaseHealth() {
+		health -= 25;
+		return health;
+	}
+	
+	public int getincreaseHealth() {
+		health += 25;
+		return health;
+	}
+
+	private int gettrollToll() {
+		return trollToll;
+	}
+
+	public int getWit() {
+		return wit;
+	}
+
+	public int getincreaseWit() {
+		wit += 10;
+		return wit;
+	}
+
+	public int getdecreaseWit() {
+		wit -= 10;
+		return wit;
+
+	}
+
+
+
+}
---
+++ src/main/java/virtualPet/VirtualPetApp.java
@@ -0,0 +1,97 @@
+package virtualPet;
+
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class VirtualPetApp {
+
+	public static void main(String[] args) {
+
+		VirtualPet virtualPet = new VirtualPet("Grendel the Troll");
+
+		Scanner input = new Scanner(System.in);
+
+		while (true) {
+
+//			int bloodLust = virtualPet.getBloodLust();
+//			int trollToll = virtualPet.getTrollToll();
+//			int wit = virtualPet.getWit();
+//			int health = virtualPet.getHealth();
+//			int increasedBloodLust = 0;
+//			int increasedTrollToll = 0;
+//			int decreasedHealth = 0;
+//			int decreasedTrollToll = 0;
+//			int increasedWit = 0;
+//			int decreasedWit = 0;
+
+			
+			System.out.println("");
+			virtualPet.displayStats();
+
+			System.out.println("");
+
+			System.out.println("Who dares cross my bridge?? Pay the toll or perish!");
+
+			int selection = 0;
+
+			// while(true) display menu of options
+			while (selection < 1 || selection > 4) {
+				System.out.println("1. Attack intruder.");
+				System.out.println("2. Demand troll toll.");
+				System.out.println("3. You can pass... if you solve this riddle.");
+				System.out.println("4. I'd rather die than barter with a troll.");
+				selection = input.nextInt();
+			}
+
+			if (selection == 1) {
+				System.out.println("You attack the intruder...");
+				int randomNum = ThreadLocalRandom.current().nextInt(1, 13);
+
+				if (randomNum > 6) {
+					System.out.println("You clobber the intruder and steal his coins.");
+					virtualPet.getincreaseBloodLust();
+					virtualPet.getincreaseTrollToll();
+				} else {
+					System.out.println("You are humilated and crawl back under your bridge.");
+					virtualPet.getdecreaseHealth();
+				}
+
+			}
+			if (selection == 2) {
+				System.out.println("If you want to pass the bridge, it will be 50 coins!!!");
+				int randomNum = ThreadLocalRandom.current().nextInt(1, 13);
+
+				if (randomNum > 6) {
+					System.out.println("The intruder quickly hands you his change purse.");
+					virtualPet.getincreaseTrollToll();
+				} else {
+					System.out.println(
+							"The intruder pushes you off the bridge and runs past. You drop some of your coins in the river.");
+					virtualPet.getdecreaseTrollToll();
+					virtualPet.getdecreaseHealth();
+				}
+
+			}
+			if (selection == 3) {
+				int randomNum = ThreadLocalRandom.current().nextInt(1, 13);
+
+				if (randomNum > 6) {
+					System.out.println("The intruder fails to answer your awesome riddle and pays you to cross.");
+					virtualPet.getincreaseWit();
+					virtualPet.getincreaseTrollToll();
+					virtualPet.getincreaseHealth();
+				} else {
+					System.out.println("The intruder answers your riddle correctly and you reluctently let him cross.");
+					virtualPet.getdecreaseWit();
+				}
+
+			}
+			if (selection == 4) {
+				System.out.println("Grendel cooks the intruder for his dinner!");
+				System.exit(0);
+			}
+			virtualPet.tick();
+		}
+
+	}
+}
